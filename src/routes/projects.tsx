5d0ae9c3e04239cac4dbdfb62fb74ba746461c2e
rasikhkp <raxiest@gmail.com>
2025-10-27 18:15:59 +0700

    Feat: Add project card in projects page

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -1,9 +1,38 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { ChevronLeft, ChevronRight, MoveLeft } from "lucide-react";
 
-export const Route = createFileRoute('/projects')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/projects")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/projects"!</div>
+	return (
+		<>
+			<Link to="/" replace={false} className="flex gap-2 mb-16">
+				<MoveLeft />
+				Back to Home
+			</Link>
+
+			<div className="pr-14 fixed top-1/2 -translate-y-1/2">
+				<div className="bg-[#262626] p-4 rounded-4xl">
+					<img src="/public/ss.png" className="rounded-2xl mb-4" />
+					<div className="text-xl text-white">Pestindo ERP</div>
+					<div className="mb-4">PT. Pestindo Mandiri Utama</div>
+
+					<button className="flex justify-center items-center w-full py-3 text-[#202020] rounded-2xl bg-white">
+						More Details
+					</button>
+				</div>
+			</div>
+
+			<div className="flex gap-2 fixed top-1/2 translate-y-44">
+				<div className="bg-white rounded-full text-[#202020] p-2">
+					<ChevronLeft />
+				</div>
+				<div className="bg-white rounded-full text-[#202020] p-2">
+					<ChevronRight />
+				</div>
+			</div>
+		</>
+	);
 }
